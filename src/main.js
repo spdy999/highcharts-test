b66b0be158c3d903cac16c581aa7e887bedddf7c
Spdy999 <1a1a1a.262626.0010@gmail.com>
2018-06-06 00:09:51 +0700

    test Highcharts dep.

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import HighchartsVue from 'highcharts-vue'
 
 Vue.config.productionTip = false
+Vue.use(HighchartsVue)
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import MyChart from '@/components/MyChart'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/helloworld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/mychart',
+      name: 'Highcharts',
+      component: MyChart
     }
   ]
 })
