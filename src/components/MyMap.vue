8dff57f782832aa325ac34369fda82044ee3dd8f
Spdy999 <1a1a1a.262626.0010@gmail.com>
2018-06-08 10:39:22 +0700

    init MyMap component

---
+++ src/components/MyMap.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="mychart">
+    <div class="title-row">
+      <p>Set the chart title here:</p>
+      <input type="text" v-model="title">
+    </div>
+    <!-- <highcharts :options="chartOptions"></highcharts> -->
+    <highcharts :constructor-type="'stockChart'" :options="chartOptions"></highcharts>
+  </div>
+</template>
+
+<script>
+import { Chart } from 'highcharts-vue'
+
+export default {
+  name: 'mychart',
+  components: {
+    highcharts: Chart
+  },
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          type: 'spline'
+        },
+        title: {
+          text: 'Entire title'
+        },
+        series: [{
+          data: [10, 0, 8, 2, 6, 4, 5, 5]
+        }]
+      },
+      title: ''
+    }
+  },
+  watch: {
+    title(newValue) {
+      if (newValue === '') {
+        this.chartOptions.title.text = 'Entire title'
+      } else {
+      	this.chartOptions.title.text = newValue
+      }
+
+    }
+  }
+
+};
+</script>
+
+<style>
+.title-row {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #eee;
+  padding: 20px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import MyChart from '@/components/MyChart'
+import MyMap from '@/components/MyMap'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/helloworld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/mymap',
+      name: 'Highmaps',
+      component: MyMap
     },
     {
       path: '/mychart',
