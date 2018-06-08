f2d92c45dcdd8502b0e8843c1340fe56fc2946a0
Spdy999 <1a1a1a.262626.0010@gmail.com>
2018-06-08 13:24:28 +0700

    test germany.geo.json map

--- src/components/MyMap.vue
+++ src/components/MyMap.vue
@@ -1,35 +1,75 @@
 <template>
-  <div id="mymap">
-    <h1>Map Test</h1>
-  </div>
+  <highcharts :constructor-type="'mapChart'" :options="mapOptions" class="map"></highcharts>
+  <!-- <highmaps :options="mapOptions"></highmaps> -->
 </template>
 
 <script>
-import { Chart } from 'highcharts-vue'
+import geojson from '../js/germany.geo.json';
 
 export default {
-  name: 'mymap',
-  components: {
-    highcharts: Chart
-  },
   data() {
     return {
-      chartOptions: {
+      mapOptions: {
         chart: {
-          type: 'spline'
+          map: geojson
         },
+
         title: {
-          text: 'Entire title'
+          text: 'GeoJSON in Highmaps'
         },
-        series: [{
-          data: [10, 0, 8, 2, 6, 4, 5, 5]
-        }]
-      },
-      title: ''
-    }
-  },
+
+        mapNavigation: {
+          enabled: true,
+          buttonOptions: {
+            verticalAlign: 'bottom'
+          }
+        },
+
+        colorAxis: {
+          tickPixelInterval: 100
+        },
+
+        series: [
+          {
+            data: [
+              ['DE.SH', 728],
+              ['DE.BE', 710],
+              ['DE.MV', 963],
+              ['DE.HB', 541],
+              ['DE.HH', 622],
+              ['DE.RP', 866],
+              ['DE.SL', 398],
+              ['DE.BY', 785],
+              ['DE.SN', 223],
+              ['DE.ST', 605],
+              ['DE.NW', 237],
+              ['DE.BW', 157],
+              ['DE.HE', 134],
+              ['DE.NI', 136],
+              ['DE.TH', 704],
+              ['DE.', 361]
+            ],
+            keys: ['code_hasc', 'value'],
+            joinBy: 'code_hasc',
+            name: 'Random data',
+            states: {
+              hover: {
+                color: '#a4edba'
+              }
+            },
+            dataLabels: {
+              enabled: true,
+              format: '{point.properties.postal}'
+            }
+          }
+        ]
+      }
+    };
+  }
 };
 </script>
-
-<style>
+ <style scoped>
+.map {
+  min-height: 500px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,14 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import HighchartsVue from 'highcharts-vue'
-import stockInit from 'highcharts/modules/stock'
+import VueHighcharts from 'highcharts-vue'
 import Highcharts from 'highcharts'
+import HighMaps from '../node_modules/highcharts/highmaps.js'
+import stockInit from 'highcharts/modules/stock'
+import mapInit from 'highcharts/modules/map'
+// import addWorldMap from './js/worldmap'
 
 Vue.config.productionTip = false
-Vue.use(HighchartsVue)
 stockInit(Highcharts)
-
+mapInit(Highcharts)
+// addWorldMap(Highcharts)
+Vue.use(VueHighcharts,  { Highcharts })
+Vue.use(HighMaps)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
